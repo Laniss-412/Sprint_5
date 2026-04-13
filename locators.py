63805337baf11b7c108a68384f33cea49f62245b
Igor Ashakov <RayarVar@yandex.ru>
2026-04-13 22:51:25 +0300

    Add new locators

--- locators.py
+++ locators.py
@@ -2,18 +2,23 @@ from selenium.webdriver.common.by import By
 
 class TestLocators:
     
-    # Регистрация
-    REG_Name_Input = (By.XPATH, "(.//input[@name = 'name'])[1]") #Поле ввода имени при регистрации
-    REG_Email_Input = (By.XPATH, "(.//input[@name = 'name'])[2]") #Поле ввода email при регистрации
-    REG_Password_Input = (By.XPATH, ".//input[@name = 'Пароль']") #Поле ввода пароля при регистрации
-    REG_Button = (By.XPATH, ".//form/button[text() = 'Зарегистрироваться']") #Кнопка Зарегистрироваться  
-    REG_Password_Error = (By.XPATH, ".//p[text() = 'Некорректный пароль']") #Ошибка некорректного пароля
+    # Форма регистрации
+    REG_Name_Input = (By.XPATH, "(.//input[@name = 'name'])[1]") #Поле ввода имени в форме регистрации
+    REG_Email_Input = (By.XPATH, "(.//input[@name = 'name'])[2]") #Поле ввода email в форме регистрации
+    REG_Password_Input = (By.XPATH, ".//input[@name = 'Пароль']") #Поле ввода пароля в форме регистрации
+    REG_Button = (By.XPATH, ".//form/button[text() = 'Зарегистрироваться']") #Кнопка Зарегистрироваться в форме регистрации 
+    REG_Password_Error = (By.XPATH, ".//p[text() = 'Некорректный пароль']") #Ошибка некорректного пароля в форме регистрации
+
+    #Форма входа
+    LOGIN_Email_Input = (By.XPATH, ".//input[@name='name']") #Поле ввода email в форме авторизации
+    LOGIN_Password_Input = (By.XPATH, ".//input[@name='Пароль']") #Поле ввода пароля в форме авторизации
+    LOGIN_Button = (By.XPATH, ".//button[text() = 'Войти']") #Кнопка входа в форме авторизации
 
     # Авторизация
-    LOGIN_Main_Button = (By.XPATH, ".//section/button[text() = 'Войти в аккаунт']") #Вход через кнопку 'Войти в аккаунт' на главной странице
-    LOGIN_Personal_Cabinet_Button = (By.XPATH, ".//p[text() = 'Личный Кабинет']") #Вход через кнопку 'Личный кабинет'
-    LOGIN_Form_Registration = (By.XPATH, ".//a[text() = 'Войти']") #Вход через кнопку 'Войти' в форме регистрации
-    LOGIN_Password_Recovery_Form = (By.XPATH, ".//a[text() = 'Войти']") #Вход через кнопку 'Войти' в форме восстановления пароля
+    LOGIN_Main_Button = (By.XPATH, ".//section/button[text() = 'Войти в аккаунт']") #Кнопка 'Войти в аккаунт' на главной странице
+    LOGIN_Personal_Cabinet_Button = (By.XPATH, ".//p[text() = 'Личный Кабинет']") #Кнопка 'Личный кабинет'
+    LOGIN_Form_Registration = (By.XPATH, ".//a[text() = 'Войти']") #Кнопка 'Войти' в форме регистрации
+    LOGIN_Password_Recovery_Form = (By.XPATH, ".//a[text() = 'Войти']") #Кнопка 'Войти' в форме восстановления пароля
 
     # Переходы
     GOTO_Personal_Cabinet_Button = (By.XPATH, ".//p[text() = 'Личный Кабинет']") #Кнопка перехода в личный кабинет
